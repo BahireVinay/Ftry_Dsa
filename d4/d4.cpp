31476135f8752a0122a3c3a5a56e9fa3eb824c94
Vinay <vinaybahire111@gmail.com>
2025-09-15 22:57:02 +0530

    d4 cum d1 commit

--- d4/d4.cpp
+++ d4/d4.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 using namespace std;
 
-// Node structure
+// Node structure for Doubly Linked List
 struct Node {
     int data;
-    Node* next;
+    Node* prev; // Pointer to previous node
+    Node* next; // Pointer to next node
 };
 
 // Insert at the beginning
 void insertAtBeginning(Node*& head, int value) {
-    Node* newNode = new Node{value, head};
+    Node* newNode = new Node{value, nullptr, head};
+    if (head) head->prev = newNode;
     head = newNode;
 }
 
 // Insert at the end
 void insertAtEnd(Node*& head, int value) {
-    Node* newNode = new Node{value, nullptr};
+    Node* newNode = new Node{value, nullptr, nullptr};
     if (!head) {
         head = newNode;
         return;
@@ -24,6 +26,7 @@ void insertAtEnd(Node*& head, int value) {
     while (temp->next)
         temp = temp->next;
     temp->next = newNode;
+    newNode->prev = temp;
 }
 
 // Insert at a specific position (1-based index)
@@ -40,7 +43,8 @@ void insertAtPosition(Node*& head, int value, int position) {
         insertAtEnd(head, value);
         return;
     }
-    Node* newNode = new Node{value, temp->next};
+    Node* newNode = new Node{value, temp, temp->next};
+    if (temp->next) temp->next->prev = newNode;
     temp->next = newNode;
 }
 
@@ -52,6 +56,7 @@ void deleteFromBeginning(Node*& head) {
     }
     Node* temp = head;
     head = head->next;
+    if (head) head->prev = nullptr;
     delete temp;
 }
 
@@ -67,10 +72,10 @@ void deleteFromEnd(Node*& head) {
         return;
     }
     Node* temp = head;
-    while (temp->next && temp->next->next)
+    while (temp->next)
         temp = temp->next;
-    delete temp->next;
-    temp->next = nullptr;
+    temp->prev->next = nullptr;
+    delete temp;
 }
 
 // Delete from a specific position (1-based index)
@@ -84,23 +89,40 @@ void deleteFromPosition(Node*& head, int position) {
         return;
     }
     Node* temp = head;
-    for (int i = 1; temp->next && i < position - 1; ++i)
+    for (int i = 1; temp && i < position; ++i)
         temp = temp->next;
-    if (!temp->next) {
+    if (!temp) {
         cout << "Position out of bounds.\n";
         return;
     }
-    Node* delNode = temp->next;
-    temp->next = delNode->next;
-    delete delNode;
+    if (temp->prev) temp->prev->next = temp->next;
+    if (temp->next) temp->next->prev = temp->prev;
+    delete temp;
 }
 
-// Display the list
-void displayList(Node* head) {
-    cout << "Linked List: ";
+// Display the list forward
+void displayForward(Node* head) {
+    cout << "Doubly Linked List (forward): ";
     while (head) {
-        cout << head->data << " -> ";
+        cout << head->data << " <-> ";
         head = head->next;
+    }
+    cout << "NULL\n";
+}
+
+// Display the list backward
+void displayBackward(Node* head) {
+    if (!head) {
+        cout << "Doubly Linked List (backward): NULL\n";
+        return;
+    }
+    // Go to the last node
+    while (head->next)
+        head = head->next;
+    cout << "Doubly Linked List (backward): ";
+    while (head) {
+        cout << head->data << " <-> ";
+        head = head->prev;
     }
     cout << "NULL\n";
 }
@@ -109,20 +131,26 @@ void displayList(Node* head) {
 int main() {
     Node* head = nullptr;
     insertAtBeginning(head, 10); // 10
-    insertAtEnd(head, 20);       // 10 -> 20
-    insertAtPosition(head, 15, 2); // 10 -> 15 -> 20
-    displayList(head);
+    insertAtEnd(head, 20);       // 10 <-> 20
+    insertAtPosition(head, 15, 2); // 10 <-> 15 <-> 20
+    displayForward(head);
+    displayBackward(head);
 
-    deleteFromBeginning(head);   // 15 -> 20
-    displayList(head);
+    deleteFromBeginning(head);   // 15 <-> 20
+    displayForward(head);
 
     deleteFromEnd(head);         // 15
-    displayList(head);
+    displayForward(head);
 
-    insertAtEnd(head, 25);       // 15 -> 25
-    insertAtEnd(head, 30);       // 15 -> 25 -> 30
-    insertAtPosition(head, 22, 2); // 15 -> 22 -> 25 -> 30
-    displayList(head);
+    insertAtEnd(head, 25);       // 15 <-> 25
+    insertAtEnd(head, 30);       // 15 <-> 25 <-> 30
+    insertAtPosition(head, 22, 2); // 15 <-> 22 <-> 25 <-> 30
+    displayForward(head);
 
-    deleteFromPosition(head, 3); // 15 -> 22 -> 30
-    displayList(head);
+    deleteFromPosition(head, 3); // 15 <-> 22 <-> 30
+    displayForward(head);
+
+    displayBackward(head);
+
+    return 0;
+}
